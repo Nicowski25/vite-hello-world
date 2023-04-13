5201a48c4e0a2528a2b3bca99e919aa00163d402
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-13 10:10:16 +0200

    hello world page

---
+++ vite-hello-world/src/components/SiteTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'SiteTitle'
+}
+</script>
+
+<template>
+
+  <h1 class="title">
+    Hello world
+  </h1>
+
+</template>
+
+<style scoped>
+.title {
+  font-size: 3rem;
+  color: red;
+}
+
+</style>
